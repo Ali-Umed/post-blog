95ac9a888d5a452f81f8f16cee4aa6aa8ce47e56
Ali-Umed <aliumed844@gmail.com>
2024-06-02 15:22:13 +0300

    publish project

---
+++ README.md
@@ -0,0 +1,54 @@
+# The  Blog
+
+The  Blog is a React application that allows users to create, search, and manage blog posts. It utilizes the PostContext to provide post data to different components.
+
+## Features
+
+- **Dark mode toggle:** Easily switch between light and dark themes.
+- **Search posts by title:** Quickly find posts by entering keywords in the search bar.
+- **Add new posts:** Create new blog posts with a title and body.
+- **Clear all posts:** Remove all existing posts with a single click.
+- **View post archive:** Access and manage archived posts.
+- **Add archive posts as new posts:** Repost archived content easily.
+- **sort posts:** sort posts by name or length easily.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Ali-Umed/post-blog.git
+   ```
+
+2. Install dependencies:
+
+   ```bash
+    npm install
+   ```
+
+3. Start the development server:
+
+   ```bash
+    npm start
+   ```
+
+4. Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Usage
+
+Toggle dark mode: Click on the sun or moon icon in the top right corner to switch themes.
+Search for posts: Type keywords in the search input field in the header to find specific posts.
+Add a new post: Fill in the title and body fields in the form and click "Add post" to create a new post.
+Clear all posts: Click the "Clear posts" button in the header to remove all posts.
+View the post archive: Click the "Show archive posts" button in the sidebar to view archived posts.
+Add an archive post as a new post: Click the "Add as new post" button next to a post in the archive to repost it.
+
+## Dependencies
+
+- React
+- faker-js
+- @faker-js/faker
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/App.js
@@ -0,0 +1,203 @@
+import { memo, useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import { PostProvider, usePosts } from "./PostContext";
+import {} from "../src/style.css";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+function App() {
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
+
+  return (
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+
+      <PostProvider>
+        <Header />
+        <Main />
+        <Archive />
+        <Footer />
+      </PostProvider>
+    </section>
+  );
+}
+
+function Header() {
+  const { onClearPosts } = usePosts();
+
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Blog
+      </h1>
+      <div>
+        <Results />
+        <SearchPosts />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+function SearchPosts() {
+  const { searchQuery, setSearchQuery } = usePosts();
+
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
+function Results() {
+  const { posts } = usePosts();
+
+  return <p>🚀 {posts.length} posts found</p>;
+}
+
+const Main = memo(function Main() {
+  return (
+    <main>
+      <FormAddPost />
+      <Posts />
+    </main>
+  );
+});
+
+function Posts() {
+  return (
+    <section>
+      <List />
+    </section>
+  );
+}
+
+function FormAddPost() {
+  const { onAddPost } = usePosts();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    if (!body || !title) return;
+    onAddPost({ title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Post title"
+      />
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        placeholder="Post body"
+      />
+      <button>Add post</button>
+    </form>
+  );
+}
+
+function List() {
+  const { posts } = usePosts();
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedPost = [...posts].sort((a, b) =>
+    sortBy === "name"
+      ? a.title.localeCompare(b.title)
+      : sortBy === "length"
+      ? a.body.length - b.body.length
+      : posts
+  );
+
+  function handleSortChange(event) {
+    setSortBy(event.target.value);
+  }
+
+  return (
+    <>
+      <div className="sort-container">
+        <label htmlFor="sort">Sort by: </label>
+        <select
+          id="sort"
+          className="sort"
+          value={sortBy}
+          onChange={handleSortChange}
+        >
+          <option value="default">Default</option>
+          <option value="name">Name</option>
+          <option value="length">Length</option>
+        </select>
+      </div>
+      <ul>
+        {sortedPost.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Archive() {
+  const { onAddPost } = usePosts();
+
+  const [posts] = useState(() =>
+    Array.from({ length: 10000 }, () => createRandomPost())
+  );
+
+  const [showArchive, setShowArchive] = useState(false);
+
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+}
+
+function Footer() {
+  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+}
+
+export default App;
